c236c586e35167b8ec6c24f1c5f3906316b95ffa
Anatolii Tsaruk <anatolii.tsaruk@gmail.com>
2019-04-10 16:08:00 +0300

    String boot application added

---
+++ src/main/java/ua/edu/ukma/termpapers/TermpapersApplication.java
@@ -0,0 +1,12 @@
+package ua.edu.ukma.termpapers;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TermpapersApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TermpapersApplication.class, args);
+	}
+}
