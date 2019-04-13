e5d7378d93bb74142b0e7b8035c9a582f4b0be1c
Oleksandr Mukhopad <alex.mukhopad@gmail.com>
2019-04-13 12:15:57 +0300

    Set project jdk to 1.8

--- src/main/java/ua/edu/ukma/termpapers/config/HBaseConfig.java
+++ src/main/java/ua/edu/ukma/termpapers/config/HBaseConfig.java
@@ -2,6 +2,8 @@ package ua.edu.ukma.termpapers.config;
 
 import static java.util.Objects.requireNonNull;
 
+import java.net.URL;
+
 import org.apache.hadoop.conf.Configuration;
 import org.apache.hadoop.hbase.HBaseConfiguration;
 import org.springframework.beans.factory.annotation.Value;
@@ -13,14 +15,14 @@ public class HBaseConfig {
   @Bean
   public Configuration hBaseConfiguration(
       @Value("${hbase.config.xml}") String configResource) {
-    var hBaseConfiguration = HBaseConfiguration.create();
+    Configuration hBaseConf = HBaseConfiguration.create();
 
-    var resourceUrl = requireNonNull(
+    URL resourceUrl = requireNonNull(
             getClass().getClassLoader().getResource(configResource),
             "HBase XML configuration file not found.");
 
-    hBaseConfiguration.addResource(resourceUrl);
+    hBaseConf.addResource(resourceUrl);
 
-    return hBaseConfiguration;
+    return hBaseConf;
   }
 }
